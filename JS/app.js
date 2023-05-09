930237edfcf30f1302226660240bf98a97061359
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-09 07:52:50 -0500

    mapped and sorted data

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,38 @@
 const taskContainer = document.querySelector(".task-container");
 
+let tasks = [
+    {
+        name: "Practice CSS Animations",
+        priority: 1
+    },
+    {
+        name: "Dev Community Work",
+        priority: 4
+    },
+    {
+        name: "Algorithm Studies",
+        priority: 3
+    }
+]
+const descendingTasks = tasks.sort((taskA, taskB) => taskA.priority - taskB.priority)
+
 
 function render() {
-   const taskBlock = document.createElement("div")
-   taskBlock.classList.add("task-block")
-   const deleteElement = document.createElement("p")
-   deleteElement.classList.add("delete-icon")
-   deleteElement.textContent = "☒"
-   const title = document.createElement("p")
-   const controller = document.createElement("button")
-   controller.classList.add("controller-button")
-   controller.textContent = "START"
-   title.textContent = "Business Class"
-   taskBlock.append(deleteElement, title, controller)
-   taskContainer.append(taskBlock)
+    descendingTasks.forEach(task => {
+        const taskBlock = document.createElement("div")
+        taskBlock.classList.add("task-block")
+        const deleteElement = document.createElement("p")
+        deleteElement.classList.add("delete-icon")
+        deleteElement.textContent = "☒"
+        const title = document.createElement("p")
+        const controller = document.createElement("button")
+        controller.classList.add("controller-button")
+        controller.textContent = "START"
+        title.textContent = task.name
+        taskBlock.append(deleteElement, title, controller)
+        taskContainer.append(taskBlock)
+    })
+ 
 }
 
 render()
